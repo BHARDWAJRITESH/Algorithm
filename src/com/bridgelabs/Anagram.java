1f185708ff379d00af92468bafb6eeea5aac6656
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-10-18 22:25:50 +0530

    Added: Take two strings as input and checking for Anagrams

---
+++ src/com/bridgelabs/Anagram.java
@@ -0,0 +1,29 @@
+package com.bridgelabs;
+
+import java.util.Arrays;
+
+public class Anagram {
+	static boolean isAnagramSort(String string1, String string2) {
+			if (string1.length() != string2.length()) {
+				return false;
+			}
+			
+			char[] a1 = string1.toCharArray();
+			char[] a2 = string2.toCharArray();
+			Arrays.sort(a1);
+			Arrays.sort(a2);
+			return Arrays.equals(a1, a2);
+	}
+
+		public static void main(String[] args) {
+			String s1 = "ritesh";
+			String s2 = "iesthr";
+			
+			if(isAnagramSort(s1.toString(),s2.toString()))
+				System.out.println("The two strings are" + " anagram of each other");
+	        else
+	            System.out.println("The two strings are not" + " anagram of each other");
+			
+		}
+
+}
